0eae9c2b19cf18a4c8667065e22dd29b9fb1873b
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-12 10:35:37 +0100

    task2 completed

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+"""
+Module that execute type-annotated function floor which takes
+a float n as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """ execute a floor program on a float arg
+
+    Args:
+    n (float): input float number to be floored
+    Return:
+    int: value of the floored float arg
+    """
+    return int(n)

cfeaf20ae2c5ec95bc16a1132b3bf8b16d4d3981
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-12 10:57:05 +0100

    task3 completed

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that execute a type-annotated function
+to_str that takes a float n as argument and
+returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function that return string
+    representation of a float number
+
+    Arg:
+    n (float): input
+    Return:
+    string: the string to be return as rep of float
+    """
+    return str(n)

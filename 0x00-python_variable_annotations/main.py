bd6db160287c87c8962f6008e7fa4d63e69ce069
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-30 08:52:23 +0100

    unitest project deleted

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-to_kv = __import__('7-to_kv').to_kv
+# matrix = []
 
-print(to_kv.__annotations__)
-print(to_kv("eggs", 3))
-print(to_kv("school", 0.02))
+# for i in range(5):
+#     matrix.append([])
+#     for j in range(5):
+#         matrix[i].append(j)
+# for k in matrix:
+#     print(k)
+
+# matrix= [[j for j in range(5)]  for i in range(5) ]
+# for k in matrix:
+#     print(k)
+
+n = int(input("Enter a number: "))
+
+matrix =[]
+for i in range(n):
+    matrix.append([i])
+    temp_list = []
+    for j in range(i + 1):
+        if j == 0 or j == i:
+            temp_list.append(1)
+        print(temp_list)
